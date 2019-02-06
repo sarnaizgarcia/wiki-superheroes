47a3f3ad80ea1c627fcfa3c4264a4540c30c9735
Silvia <sarnaizgarcia@gmail.com>
2019-02-06 11:37:06 +0100

    Creo el store por defecto
    Issue #1
    
    * He creado el store por defecto

---
+++ src/reducers/default-state.js
@@ -0,0 +1,121 @@
+const defaultState = {
+  tableHeroes: {
+    page: 0,
+    size: 5,
+    lastSize: 4,
+    columns: [
+      {
+        id: 0,
+        titulo: "Nombre",
+        dato: "nombre"
+      },
+      {
+        id: 1,
+        titulo: "Historia",
+        dato: "historia"
+      }
+    ]
+  },
+  dataTableHeroes: [
+    {
+      id: 0,
+      nombre: "Superopaco",
+      historia: "No deja pasar la luz a través de él"
+    },
+    {
+      id: 1,
+      nombre: "Supertaxista",
+      historia: "No deja pasar a los VTCs a través de sí"
+    },
+    {
+      id: 2,
+      nombre: "Supermercado",
+      historia: "Aglutina el mayor número de ofertas posibles de 3x2"
+    },
+    {
+      id: 3,
+      nombre: "Superbanquero",
+      historia: "Los banqueros son gente buena y honrada"
+    },
+    {
+      id: 4,
+      nombre: "Superpesado",
+      historia: "Domina la técnica del martillo pilón"
+    },
+    {
+      id: 5,
+      nombre: "Súper",
+      historia: "El mejor"
+    },
+    {
+      id: 6,
+      nombre: "Superheroe 7",
+      historia: "Cualidades de superheroe 7"
+    },
+    {
+      id: 7,
+      nombre: "Superheroe 8",
+      historia: "Cualidades de superheroe 8"
+    },
+    {
+      id: 8,
+      nombre: "Superheroe 9",
+      historia: "Cualidades de superheroe 9"
+    },
+    {
+      id: 9,
+      nombre: "Superheroe 10",
+      historia: "Cualidades de superheroe 10"
+    },
+    {
+      id: 10,
+      nombre: "Superheroe 11",
+      historia: "Cualidades de superheroe 11"
+    },
+    {
+      id: 11,
+      nombre: "Superheroe 12",
+      historia: "Cualidades de superheroe 12"
+    },
+    {
+      id: 12,
+      nombre: "Superheroe 13",
+      historia: "Cualidades de superheroe 13"
+    },
+    {
+      id: 13,
+      nombre: "Superheroe 14",
+      historia: "Cualidades de superheroe 14"
+    },
+    {
+      id: 14,
+      nombre: "Superheroe 15",
+      historia: "Cualidades de superheroe 15"
+    },
+    {
+      id: 15,
+      nombre: "Superheroe 16",
+      historia: "Cualidades de superheroe 16"
+    },
+    {
+      id: 16,
+      nombre: "Superheroe 17",
+      historia: "Cualidades de superheroe 17"
+    },
+    {
+      id: 17,
+      nombre: "Superheroe 18",
+      historia: "Cualidades de superheroe 18"
+    },
+    {
+      id: 18,
+      nombre: "Superheroe 19",
+      historia: "Cualidades de superheroe 19"
+    },
+    {
+      id: 19,
+      nombre: "Superheroe 20",
+      historia: "Cualidades de superheroe 20"
+    }
+  ]
+};
